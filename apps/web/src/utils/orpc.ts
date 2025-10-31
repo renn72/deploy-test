890d1c1fe4a77b6f6cdf15e623b301a8afbd1da2
renn <renn@warner.systems>
2025-10-31 18:58:55 +1100

    cors

--- apps/web/src/utils/orpc.ts
+++ apps/web/src/utils/orpc.ts
@@ -1,9 +1,9 @@
+import type { AppRouterClient } from "@deploy-test/api/routers/index";
 import { createORPCClient } from "@orpc/client";
 import { RPCLink } from "@orpc/client/fetch";
 import { createTanstackQueryUtils } from "@orpc/tanstack-query";
 import { QueryCache, QueryClient } from "@tanstack/react-query";
 import { toast } from "sonner";
-import type { AppRouterClient } from "@deploy-test/api/routers/index";
 
 export const queryClient = new QueryClient({
 	queryCache: new QueryCache({
@@ -22,6 +22,12 @@ export const queryClient = new QueryClient({
 
 export const link = new RPCLink({
 	url: `${import.meta.env.VITE_SERVER_URL}/rpc`,
+  // add headers for cors
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+    "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization",
+  },
 	fetch(url, options) {
 		return fetch(url, {
 			...options,
