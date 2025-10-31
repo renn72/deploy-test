38052f11005306691bbd7332b65baa2fd04bbcdc
renn <renn@warner.systems>
2025-10-31 19:31:52 +1100

    remove vite cors

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -6,6 +6,9 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
 	plugins: [tailwindcss(), tanstackRouter({}), react()],
+  server: {
+    cors: false,
+  },
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
